571384489160300f6729818795e42a1e26eaf328
kiryu1120 <138645777+kiryu1120@users.noreply.github.com>
2024-05-31 11:24:46 +0900

    Update Bullet.cs

--- Bullet.cs
+++ Bullet.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Playerの弾
 public class Bullet : MonoBehaviour
 {
     // Start is called before the first frame update
